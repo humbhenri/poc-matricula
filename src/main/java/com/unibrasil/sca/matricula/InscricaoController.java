a01d72d65252137f78a4e9e4ca7872a96c881536
humbhenri <humbhenri@humbhenri-MS-7994>
2017-07-29 16:05:22 -0300

    Classe para enviar erros de validação

--- src/main/java/com/unibrasil/sca/matricula/InscricaoController.java
+++ src/main/java/com/unibrasil/sca/matricula/InscricaoController.java
@@ -1,8 +1,6 @@
 package com.unibrasil.sca.matricula;
 
 import java.net.URI;
-import java.util.List;
-import java.util.Locale;
 
 import javax.validation.Valid;
 
@@ -10,19 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.config.ResourceNotFoundException;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -74,20 +65,4 @@ public class InscricaoController {
 		}
 	}
 	
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ValidationErrorDTO processValidationError(MethodArgumentNotValidException ex) {
-        BindingResult result = ex.getBindingResult();
-        List<FieldError> fieldErrors = result.getFieldErrors();
-        return processFieldErrors(fieldErrors);
-    }
- 
-    private ValidationErrorDTO processFieldErrors(List<FieldError> fieldErrors) {
-        ValidationErrorDTO dto = new ValidationErrorDTO();
-        for (FieldError fieldError: fieldErrors) {
-            String errorMessage = fieldError.getDefaultMessage();
-            dto.addFieldError(fieldError.getField(), errorMessage);
-        }
-        return dto;
-    }
 }
